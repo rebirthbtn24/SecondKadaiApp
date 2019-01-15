1cf079790ac20e56358286b3106a5f5f74a5d310
rebirthbtn24 <rebirthbtn@gmail.com>
2019-01-15 16:02:26 +0900

    画面作成が完了

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
